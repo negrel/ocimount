d2f64b7256b8a05809a2b338e1d3c4b1987666cd
Alexandre Negrel <negrel.dev@protonmail.com>
2022-10-16 20:45:02 +0100

    feat: add umount command

---
+++ cmd/ocimount/umount.go
@@ -0,0 +1,57 @@
+package ocimount
+
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(umountCmd)
+	pflag := umountCmd.PersistentFlags()
+	setupStoreOptionsFlags(pflag)
+	setupLogrusFlags(pflag)
+
+	pflag.BoolP("force", "f", false, "force the unmount of an image")
+}
+
+var umountCmd = &cobra.Command{
+	Use:   "umount",
+	Short: "Unmount an OCI/Docker image.",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		if len(args) != 1 {
+			return errors.New("expecting exactly one argument: an OCI/Docker image reference")
+		}
+
+		flags := cmd.Flags()
+		force, err := flags.GetBool("force")
+		if err != nil {
+			return
+		}
+
+		return umount(args[0], force)
+	},
+}
+
+func umount(imgRefStr string, force bool) (err error) {
+	logrus.Debugf("unmounting %q...", imgRefStr)
+
+	store, err := containersStore()
+	if err != nil {
+		return
+	}
+
+	imgRef, err := parseReference(imgRefStr)
+	if err != nil {
+		return
+	}
+
+	_, err = store.UnmountImage(imgRef.String(), force)
+	if err != nil {
+		return
+	}
+	logrus.Infof("%q successfully unmounted.", imgRef)
+
+	return
+}
